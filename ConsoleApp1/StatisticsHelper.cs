1a3b139bc8fc0818b0c6a58bd1699111cf279e97
s34540-code <s34540@pjwstk.edu.pl>
2026-03-17 23:00:49 +0100

    Add CalculateAverage method in StaticHelper

--- ConsoleApp1/StatisticsHelper.cs
+++ ConsoleApp1/StatisticsHelper.cs
@@ -2,5 +2,15 @@ namespace ConsoleApp1;
 
 public class StatisticsHelper
 {
+    public static double CalculateAverage(int[] values)
+    {
+        if (values == null || values.Length == 0)
+            return 0;
     
+        double sum = 0;
+        foreach (var v in values)
+            sum += v;
+
+        return sum / values.Length;
+    }
 }
